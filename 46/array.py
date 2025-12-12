9c516183166f717e1567ff38911fdc01057210e8
Shriganesh-Bayar <sgbayar2005@gmail.com>
2025-12-12 23:43:31 +0530

    Coding challenge 46, closes #50

---
+++ 46/array.py
@@ -0,0 +1,34 @@
+class Negative_Number(Exception):
+    '''Raised when the entered number is negative '''
+    pass
+
+def Enter_Input() -> int:
+    while True:
+        try:
+            number = int(input("Enter the number of elements: "))
+            if number <= 0 :
+                raise Negative_Number("Number should be Postive integer!")
+            return number
+        except Negative_Number:
+            print(Negative_Number)
+        except ValueError:
+            print("Number should be Postive integer!")
+
+# this takes exactly size number of input ofr array
+def Enter_array(size: int) -> list[float]:
+    while True:
+        try:
+            arr = list(map(float, input("Enter array elements: ").split()))
+            if len(arr) != size:
+                print(f"Please enter exactly {size} elements!")
+                continue
+            return arr
+        except ValueError:
+            print("All elements must be integers!")
+
+if __name__ == "__main__":
+    n = Enter_Input()
+    arr = Enter_array(n)
+    sum = sum(arr)
+    print("Total sum of array is:", sum)
+    
