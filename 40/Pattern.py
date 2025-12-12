c7ab8cfdc3fe6d4b2fa42f56abee80c47f4adcf0
Shriganesh-Bayar <sgbayar2005@gmail.com>
2025-12-12 23:42:33 +0530

    Coding challenge 40, closes #44

---
+++ 40/Pattern.py
@@ -0,0 +1,26 @@
+class Negative_Number(Exception):
+    '''Raised when the entered number is negative '''
+    pass
+
+def Enter_Input() -> int:
+    while True:
+        try:
+            number = int(input("Enter the number: "))
+            if number <= 0 :
+                raise Negative_Number("Number should be Postive integer!")
+            return number
+        except Negative_Number:
+            print(Negative_Number)
+        except ValueError:
+            print("Number should be Postive integer!")
+
+if __name__ == "__main__":
+    n = Enter_Input()
+    multiplier = 1
+    prev = 1
+    for i in range(n):
+        for j in range(i + 1):
+            prev *= multiplier
+            print(prev, end = " ")
+            multiplier += 1
+        print()
