f0957611ea2978386e18b4046f3a18c841683211
Shriganesh-Bayar <sgbayar2005@gmail.com>
2025-12-14 16:15:00 +0530

    Coding Challenge 13, closes #14

--- Tax_Collector/Tax_Collector.py
+++ Tax_Collector/Tax_Collector.py
@@ -7,6 +7,7 @@ class Employee:
     gross_monthly_salary: float
     annual_gross_salary: float
     taxable_annual_income: float
+    total_tax: float
 
     def __init__(self, name, EmpID, basic_salary, special_allowance, bonus_percentage):
         self.name = name
@@ -32,10 +33,52 @@ class Employee:
         print(f"Annual gross salary: ₹{self.annual_gross_salary}")
         print("Taxable Income: ₹50000")
         print(f"The Taxable annual income: ₹{self.taxable_annual_income}")
-        
+
+    def calculate_tax(self):
+        tax = 0
+        if self.taxable_annual_income <= 700000:
+            print("Taxable income is not more than 7lakh, hence no tax")
+            pass
+
+        # ₹0-₹300000
+        if self.taxable_annual_income >= 300000:
+            slab_tax = min(self.taxable_annual_income - 300000, 300000) * 0.05
+            tax += slab_tax
+            print("₹300000-₹600000 ->", slab_tax)
+
+        # ₹0-₹300000
+        if self.taxable_annual_income >= 600000:
+            slab_tax = min(self.taxable_annual_income - 300000, 300000) * 0.1
+            tax += slab_tax
+            print("₹600000-₹900000 ->", slab_tax)
+
+        # ₹0-₹300000
+        if self.taxable_annual_income >= 900000:
+            slab_tax = min(self.taxable_annual_income - 300000, 300000) * 0.15
+            tax += slab_tax
+            print("₹900000-₹1200000 ->", slab_tax)
+
+        # ₹0-₹300000
+        if self.taxable_annual_income >= 1200000:
+            slab_tax = min(self.taxable_annual_income - 300000, 300000) * 0.2
+            tax += slab_tax
+            print("₹1200000-₹1500000 ->", slab_tax)
+
+        # ₹0-₹300000
+        if self.taxable_annual_income >= 1500000:
+            slab_tax = min(self.taxable_annual_income - 300000, 300000) * 0.3
+            tax += slab_tax
+            print("Above ₹300000 ->", slab_tax)
+
+        education_health = tax * 0.04
+        self.total_tax = tax + education_health
+        print("Tax:", tax)
+        print("Tax on education and health: ", education_health)
+        print("Total tax:", self.total_tax)        
         
 if __name__ == "__main__":
     n = int(input("Enter number of employee: "))
+    id = {}
     employee = [None] * n
     for i in range(n):
         name = input("Enter the name of employee: ")
@@ -44,6 +87,9 @@ if __name__ == "__main__":
         special_allowance = float(input(f"Enter the special allowance of {name}: "))
         bonus_percentage = float(input(f"Enter the bonus percentage(%): "))
         employee[i] = Employee(name, EmpId, basic_salary, special_allowance, bonus_percentage)
+        id[EmpId] = i
+
     for i in range(n):
         employee[i].Report()
         employee[i].Taxable_Income()
+        employee[i].calculate_tax()
