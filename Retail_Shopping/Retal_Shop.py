477bff520f93a2d1cd431f2bd48c34d0c7bbb6c8
Shriganesh-Bayar <sgbayar2005@gmail.com>
2025-12-15 19:40:22 +0530

    Coding Challenge 25, closes #28

--- Retail_Shopping/Retal_Shop.py
+++ Retail_Shopping/Retal_Shop.py
@@ -22,11 +22,35 @@ class Item:
         print("Price per item: ₹", self.price)
         print("Total Cost: ₹", self.total_cost())
 
-if __name__ == "__main__":
-    code = input("Enter item code: ")
-    description = input("Enter item description: ")
-    quantity = int(input("Enter quantity: "))
-    price = float(input("Enter price per item (₹): "))
+    @staticmethod
+    def invoice(items):
+        grand_total = 0
 
-    item = Item(code, description, quantity, price)
-    item.display()
+        print("\nInvoice")
+        print("----------------------------------------------------")
+        print(f"{'Code':10} {'Description':15} {'Qty':8} {'Price':10} {'Total':10}")
+        print("----------------------------------------------------")
+
+        for item in items:
+            print(f"{item.code:10} {item.description:10} {item.quantity:8} ₹{item.price:8.2f} ₹{item.total_cost():10.2f}")
+            grand_total += item.total_cost()
+
+        print("----------------------------------------------------")
+        print(f"Grand Total: ₹{grand_total:.2f}")
+
+if __name__ == "__main__":
+    items = []
+    code_to_index = {}
+    n = int(input("Enter the number of items: "))
+
+    for i in range(n):
+        print("Enter the details of item", i + 1, ": ")
+        code = input("Item Code: ")
+        description = input("Description: ")
+        quantity = int(input("Quantity: "))
+        price = float(input("Price per item (₹): "))
+
+        items.append(Item(code, description, quantity, price))
+        code_to_index["code"] = i
+
+    Item.invoice(items)    
